01500c6181239890aa3cd76d3761189cdf27f979
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-06-15 16:54:32 +0900

    반응형 헤더까지 완성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,79 @@
 /* eslint-disable */
 import Link from 'next/link'
 import ToggleBtn from './Toggle-Button'
+import { useCallback, useState } from 'react'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenuOpen = useCallback(() => {
+    setIsMenuOpen((prev) => !prev)
+  }, [])
+
   return (
     <>
-      <header className="bg-purple-400 text-gray-600 body-font dark:bg-slate-800">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link
-            href="/"
-            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+      <nav className="bg-purple-400 text-gray-600 body-font dark:bg-slate-800 dark:border-gray-700">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link href="/" className="flex items-center">
+            <span className="self-center text-2xl font-semibold text-white whitespace-nowrap dark:text-white">
+              SCS's Portfolio
+            </span>
+          </Link>
+          <button
+            data-collapse-toggle="navbar-solid-bg"
+            type="button"
+            onClick={toggleMenuOpen}
+            className="inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:bg-gray-100 hover:text-rose-400 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-solid-bg"
+            aria-expanded="false"
           >
             <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-10 h-10 text-white p-2 bg-purple-500 rounded-full"
-              viewBox="0 0 24 24"
             >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
             </svg>
-            <span className="text-white ml-3 text-xl">SCS's Portfolio</span>
-          </Link>
-          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link
-              href="/projects"
-              className="text-white mr-5 hover:text-pink-900 font-bold"
-            >
-              내 프로젝트
-            </Link>
-            <Link
-              href="/contact"
-              className="text-white mr-5 hover:text-pink-900 font-bold"
-            >
-              연락처
-            </Link>
-          </nav>
-          <ToggleBtn />
+          </button>
+          <div
+            className={`${
+              isMenuOpen ? 'hidden' : ''
+            } w-full md:block md:w-auto text-center`}
+            id="navbar-solid-bg"
+          >
+            <ul className="flex flex-col items-center font-medium mt-4 rounded-lg bg-purple-400 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+              <li>
+                <Link
+                  href="/projects"
+                  className="block py-2 pl-3 pr-4 text-white font-bold rounded hover:bg-gray-100 hover:text-rose-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  aria-current="page"
+                >
+                  내 프로젝트
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/contact"
+                  className="block py-2 pl-3 pr-4 text-white font-bold rounded hover:bg-gray-100 hover:text-rose-400 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  연락처
+                </Link>
+              </li>
+              <li>
+                <span className="block rounded md:hover:bg-transparent md:border-0">
+                  <ToggleBtn />
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
-      </header>
+      </nav>
     </>
   )
 }
