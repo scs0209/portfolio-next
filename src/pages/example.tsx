83e235c3f38b1e1043c82368f6bb03ea56e27cfb
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-11-17 15:59:47 +0900

    feat: 메인페이지에 지구 모양 추가

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -4,43 +4,80 @@ import {
   OrbitControls,
   Center,
   Stars,
-  Float,
   Sparkles,
   useMatcapTexture,
+  Preload,
 } from '@react-three/drei'
-import { Canvas, useThree } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Physics } from '@react-three/cannon'
+import { Vector3 } from 'three'
 import ProjectBtn from '@/components/Main/ProjectBtn'
+import Earth from '@/components/animation/Earth'
+import CanvasLoader from '@/components/Loader'
+
+function CircularText({ text, radius, rotationSpeed, matcapTexture }: any) {
+  const groupRef = useRef<any>()
+
+  const { width: w, height: h } = useThree((state) => state.viewport)
+
+  // 텍스트를 각 글자로 분리합니다.
+  const characters = text.split('')
+
+  useFrame(({ clock }) => {
+    if (groupRef.current) {
+      // 각 글자를 원형 경로에 배치합니다.
+      characters.forEach((char: string, i: number) => {
+        const radian =
+          ((Math.PI * 2) / characters.length) * i -
+          clock.getElapsedTime() * rotationSpeed
+        const xPos = Math.cos(radian) * radius
+        const zPos = Math.sin(radian) * radius
+
+        const textMesh = groupRef.current.children[i]
+        textMesh.position.set(xPos, 0, zPos)
+        textMesh.lookAt(new Vector3(0, 0, 0))
+      })
+    }
+  })
+
+  return (
+    <group ref={groupRef} position={[-1, 0, 0]}>
+      {characters.map((char: string, i: number) => (
+        <Text3D
+          key={i}
+          position={[0, 0, 0]}
+          scale={[-1, 1, 1]}
+          size={w / 50}
+          font="/gt.json"
+          curveSegments={24}
+          bevelSegments={1}
+          bevelEnabled
+          bevelSize={0.08}
+          bevelThickness={0.03}
+          height={1}
+          lineHeight={0.9}
+          letterSpacing={0.3}
+        >
+          {char}
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </Text3D>
+      ))}
+    </group>
+  )
+}
 
 function Hero() {
   const [matcapTexture] = useMatcapTexture('F77777_FBE1E1_FAB2B2_FBC4C4')
-  const ref = useRef<any>()
-
-  const { width: w, height: h } = useThree((state) => state.viewport)
 
   return (
     <Center scale={[0.9, 1, 1]}>
       <Physics>
-        <Float speed={1}>
-          <Text3D
-            position={[0, 0, -10]}
-            scale={[-1, 1, 1]}
-            ref={ref}
-            size={w / 20}
-            font="/gt.json"
-            curveSegments={24}
-            bevelSegments={1}
-            bevelEnabled
-            bevelSize={0.08}
-            bevelThickness={0.03}
-            height={1}
-            lineHeight={0.9}
-            letterSpacing={0.3}
-          >
-            {`HELLO THIS IS \n SCS's WORLD`}
-            <meshMatcapMaterial matcap={matcapTexture} />
-          </Text3D>
-        </Float>
+        <CircularText
+          text={`HELLO THIS IS SCS's WORLD `}
+          radius={7}
+          rotationSpeed={1}
+          matcapTexture={matcapTexture}
+        />
       </Physics>
     </Center>
   )
@@ -49,17 +86,24 @@ function Hero() {
 export default function Example() {
   return (
     <div className="scene">
-      <Canvas camera={{ position: [0, 0, -10], fov: 60 }}>
+      <Canvas
+        shadows
+        frameloop="demand"
+        dpr={[1, 2]}
+        gl={{ preserveDrawingBuffer: true }}
+        camera={{ position: [0, 0, -15], fov: 60 }}
+      >
         <OrbitControls
           enableZoom
           autoRotate
-          autoRotateSpeed={-0.1}
+          autoRotateSpeed={1}
           enablePan
           zoomSpeed={0.15}
           dampingFactor={0.05}
         />
 
-        <Suspense fallback="Loading">
+        <Suspense fallback={<CanvasLoader />}>
+          <Earth />
           <Stars
             radius={100}
             depth={100}
@@ -78,9 +122,11 @@ export default function Example() {
             color="#fff3b0"
           />
           <Hero />
+          <Preload all />
         </Suspense>
         <ambientLight intensity={1} color="#dee2ff" />
       </Canvas>
+
       <div className="flex justify-center relative">
         <ProjectBtn />
       </div>
