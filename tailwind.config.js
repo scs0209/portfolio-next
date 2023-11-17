a4e708299c1156d9e72c170f275b2bb01d097e0f
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-11-17 17:54:52 +0900

    fix: bg-image 안나오는 에러 해결 테스트(11)

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,8 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'custom-gradient':
+          'linear-gradient(180deg, hwb(200deg 0% 98%) 1%, hsl(200deg 100% 3%) 17%, hsl(200deg 100% 6%) 26%, hsl(200deg 100% 9%) 37%, hsl(200deg 100% 12%) 46%, hsl(200deg 100% 15%) 53%, hsl(200deg 100% 18%) 59%, hsl(200deg 100% 21%) 65%, hsl(200deg 100% 24%) 70%, hsl(200deg 100% 27%) 74%, hsl(200deg 100% 30%) 78%, hsl(200deg 100% 33%) 81%, hsl(200deg 100% 36%) 84%, hsl(200deg 100% 39%) 87%, hsl(200deg 100% 42%) 89%, hsl(200deg 100% 45%) 91%, hsl(200deg 100% 48%) 93%, hsl(200deg 100% 51%) 95%, hsl(200deg 100% 54%) 96%, hsl(200deg 100% 57%) 97%, hsl(200deg 100% 60%) 98%, hsl(200deg 100% 63%) 99%, hsl(200deg 100% 66%) 100%)',
         explosion: 'url("/bg-explosion.png")',
         circles: 'url("/bg-circles.png")',
         circleStar: 'url("/circle-star.svg")',
