99d25bc790f61edf608238f769fb410496e2fd8e
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-11-22 01:35:01 +0900

    style: 카드 컴포넌트 내용 적용

--- src/components/projects/ImageSlider.tsx
+++ src/components/projects/ImageSlider.tsx
@@ -1,13 +1,46 @@
 import { motion } from 'framer-motion'
 import React, { useState } from 'react'
 import Modal from '../common/Modal'
+import { Tag } from '@/types'
+import ProjectTag from './project-tag'
 
 const ImageSlider = ({ data, index, positionIndexes }: any) => {
   console.log(data)
   const [show, setShow] = useState(false)
   const imgSrc = data.cover?.file?.url || data.cover?.external?.url
+  const githubLink = data.properties.github.url
   const projectTitle = data.properties.name.title[0].plain_text
   const description = data.properties.description.rich_text[0].plain_text
+  const tags: Tag[] = data.properties.tag.multi_select
+  const { start } = data.properties.workPeriod.date
+  let { end } = data.properties.workPeriod.date
+  if (!end) {
+    end = '작업 중'
+  }
+
+  const diffDate = (start: any, end: any) => {
+    const startDateStringArray = start.split('-')
+    const endDateStringArray = end.split('-')
+
+    const startDate = new Date(
+      startDateStringArray[0],
+      startDateStringArray[1],
+      startDateStringArray[2],
+    )
+    const endDate = new Date(
+      endDateStringArray[0],
+      endDateStringArray[1],
+      endDateStringArray[2],
+    )
+
+    const diffInMs = Math.abs(Number(endDate) - Number(startDate))
+    const result = diffInMs / (1000 * 60 * 60 * 24)
+    if (Number.isNaN(result)) {
+      return '작업 중'
+    }
+
+    return `${result}일`
+  }
 
   const positions = ['center', 'left1', 'left', 'right', 'right1']
 
@@ -71,16 +104,32 @@ const ImageSlider = ({ data, index, positionIndexes }: any) => {
         initial="center"
         variants={imageVariants}
         transition={{ duration: 0.5 }}
-        style={{ width: '20%', position: 'absolute', height: '40%' }}
+        className="overflow-hidden absolute bg-[#343444] rounded-[12px] p-4"
+        style={{ width: '20%', height: '40%' }}
         onClick={handleImageClick}
       >
         <motion.img
           key={index}
           src={imgSrc}
           alt={projectTitle}
-          className="rounded-[12px]"
-          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          className="rounded-[12px] overflow-hidden"
+          style={{ width: '100%', height: '70%', objectFit: 'cover' }}
         />
+        <span className="text-sm">
+          {`${start} ~ ${end}`} (총: {diffDate(start, end)})
+        </span>
+        <div>{projectTitle}</div>
+        <div className="text-sm">{description}</div>
+        <a href={githubLink} target="_blank" className="mt-3 mb-2">
+          <span className="px-3 py-0.5 text-sm fond-medium rounded-lg text-white border bg-gray-500 border-gray-500 hover:bg-pink-400 dark:border-gray-500 dark:bg-gray-500 dark:hover:text-black dark:hover:border-black">
+            Github
+          </span>
+        </a>
+        <div className="flex flex-row flex-wrap">
+          {tags.map((tag: Tag) => {
+            return <ProjectTag key={tag.id} tag={tag} />
+          })}
+        </div>
       </motion.div>
       <Modal showModal={show} setShowModal={setShow}>
         {renderPageContent()}
