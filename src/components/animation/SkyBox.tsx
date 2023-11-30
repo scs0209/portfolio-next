f338cde6cbf71add548d9a238b079d9ceffc1347
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-12-01 02:57:32 +0900

    style: 배경 변경

--- src/components/animation/SkyBox.tsx
+++ src/components/animation/SkyBox.tsx
@@ -6,12 +6,12 @@ const SkyBox = () => {
   const { scene } = useThree()
   const loader = new CubeTextureLoader()
   const texture = loader.load([
-    './Yokohama3/material_emissive.png',
-    './Yokohama3/material_emissive.png',
-    './Yokohama3/material_emissive.png',
-    './Yokohama3/material_emissive.png',
-    './Yokohama3/material_emissive.png',
-    './Yokohama3/material_emissive.png',
+    './Yokohama3/px.jpg',
+    './Yokohama3/nx.jpg',
+    './Yokohama3/py.jpg',
+    './Yokohama3/ny.jpg',
+    './Yokohama3/pz.jpg',
+    './Yokohama3/nz.jpg',
   ])
   scene.background = texture
   return null
